0f4a82c964a3ae14dc1209882d7204c40688792a
jessidi <djxhwill@gmail.com>
2020-05-20 07:47:32 -0700

    add query

--- backend/src/Query.js
+++ backend/src/Query.js
@@ -1,2 +1,7 @@
-const Query = {};
+const Query = {
+  dogs(parent, args, ctx, info) {
+    global.dogs = global.dogs || [];
+    return global.dogs;
+  },
+};
 module.exports = Query;
--- backend/src/createServer.js
+++ backend/src/createServer.js
@@ -1,7 +1,7 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-const Mutation = require("./resolvers/Mutation");
-const Query = require("./resolvers/Query");
+const Mutation = require("./Mutation");
+const Query = require("./Query");
 const db = require("./db");
 
 // Create the GraphQL Yoga Server
